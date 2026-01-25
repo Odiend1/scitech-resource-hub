7729d8be15f81c23ba50587e5831e45b58d9a682
Odiend1 <diendaosric@gmail.com>
2026-01-25 16:12:14 -0600

    Added basic search functionality

--- resource-directory/script.js
+++ resource-directory/script.js
@@ -1,5 +1,22 @@
 
 
 function searchBarFilter(){
-    
+    searchQuery = document.getElementById("search-bar").value.toLowerCase().trim();
+    let resourceItems = document.getElementsByClassName("resource-item");
+
+    for(i = 0; i < resourceItems.length; i++){
+        let item = resourceItems[i];
+        let itemTitle = item.querySelector("a").innerHTML.toLowerCase().trim();
+        let itemDesc = item.querySelector("p").innerHTML.toLowerCase().trim();
+
+        if(itemTitle.includes(searchQuery)){
+            item.style.display = "inline-block";
+        }
+        else if(searchQuery.length > 4 && itemDesc.includes(searchQuery)){
+            item.style.display = "inline-block";
+        }
+        else{
+            item.style.display = "none";
+        }
+    }
 }
