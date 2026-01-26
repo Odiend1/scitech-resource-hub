8bba8ca731eb57bcd9edb20da3d9bb23bb0c1a96
Odiend1 <diendaosric@gmail.com>
2026-01-25 21:04:55 -0600

    Made tag groups have color automatically

--- resource-directory/script.js
+++ resource-directory/script.js
@@ -60,12 +60,44 @@ function populateResources(){
                 tag.className = `tag ${tagGroup}-tag`;
                 tag.innerHTML = resources[i].tags[tagGroup][j];
                 tagContainer.appendChild(tag);
+                tag.style.backgroundColor = tagColorFromString(tagGroup);
             }
         }
         resourceItem.appendChild(tagContainer);
 
         resourceList.appendChild(resourceItem);
     }
+}
+
+function tagColorFromString(str){
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash << 5) - hash + str.charCodeAt(i);
+        hash |= 0;
+    }
+    
+    hash = Math.abs(hash);
+
+    function hslToHex(h, s, l) {
+        s /= 100;
+        l /= 100;
+
+        const k = n => (n + h / 30) % 12;
+        const a = s * Math.min(l, 1 - l);
+        const f = n =>
+            l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+
+        const toHex = x =>
+            Math.round(255 * x).toString(16).padStart(2, "0");
+
+        return `#${toHex(f(0))}${toHex(f(8))}${toHex(f(4))}`;
+    }
+
+    const hue = hash % 360;
+    const saturation = 55 + (hash % 15);
+    const lightness  = 72 + (hash % 8);
+
+    return hslToHex(hue, saturation, lightness)
 }
 
 function applyFilters(){
